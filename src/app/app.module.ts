000dd1ff504bd49a4800d9fa2d140e1b94536a7c
DCarolia <carolina.dominguez2217@gmail.com>
2023-02-16 23:22:29 -0300

    Graficos de Skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
-import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
 import { LogrosComponent } from './components/logros/logros.component';
 import { ExperienciaYEducacionComponent } from './components/experiencia-y-educacion/experiencia-y-educacion.component';
 import { AppRoutingModuleTsModule } from './app-routing.module.ts/app-routing.module.ts.module';
 import { LogoYRedesComponent } from './components/logo-y-redes/logo-y-redes.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -20,10 +21,13 @@ import { LogoYRedesComponent } from './components/logo-y-redes/logo-y-redes.comp
     ExperienciaYEducacionComponent,
     LogoYRedesComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModuleTsModule
+    AppRoutingModuleTsModule,
+    NgCircleProgressModule.forRoot({})
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
